02147c9c0c0bf9fc1b9eeb772e81ebf660d8c0ee
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-04 16:01:57 -0500

    fleshed out book model

---
+++ LendingLibrary/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LendingLibrary.Models
+{
+    public class Author
+    {
+        public Author(string firstName, string lastName)
+        {
+                this.FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+
+            this.LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+}
+   
--- LendingLibrary/Models/Book.cs
+++ LendingLibrary/Models/Book.cs
@@ -1,10 +1,16 @@
 ﻿namespace LendingLibrary.Models
-
 {
 
     public class Book
     {
 
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        //Make a constructor
     }
 
 }
